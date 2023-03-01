656e5cca66017cccc7cb455e2f697baa22e10922
ARBUCHELI <radio_seattle@hotmail.com>
2023-03-01 15:57:19 -0500

    cofiguring the router

--- Exercise Files/Ch07/07_01/start/react-app/src/index.js
+++ Exercise Files/Ch07/07_01/start/react-app/src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { App } from "./App";
+import { About } from "./App";
+import { Contact } from "./App";
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
-    <App />,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+   ,
   document.getElementById("root")
 );
 
